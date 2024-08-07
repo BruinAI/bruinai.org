3a0ef63c0ca2035aece17a1aa1391a78d522330f
Toidi357 <48337299+Toidi357@users.noreply.github.com>
2024-08-07 15:57:43 +0800

    Create urlrewrite.py

---
+++ urlrewrite.py
@@ -0,0 +1,25 @@
+import sys
+
+def process_html(file_path):
+  content = ''
+  print(f'Processing {file_path}')
+  
+  with open(file_path, 'r') as f:
+    content = f.read()
+
+    # use javascript window to perform a URL rewrite
+    # essentially if user requests /about.html, it will show /about in the URL bar
+    if 'index.html' in file_path:
+      content = content.replace('</head>', "<script>if (window.location.pathname === '/index.html') {window.history.pushState({}, '', '/')};</script></head>")
+    else:
+      content = content.replace('</head>', f"<script>if (window.location.pathname === '/{file_path}') \{window.history.pushState(\{}, '', '/{file_path[-5]}')};</script></head>")
+
+  with open(file_path, 'w') as f:
+    f.write(content)
+          
+
+if __name__ == "__main__":
+  if len(sys.argv) != 2:
+    print("Usage: python process_html.py <file_path>")
+    sys.exit(1)
+  process_html(sys.argv[1])
