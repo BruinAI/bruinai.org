f25228b03fa66f1f617e00a8e9a0c44465bda89f
Kevin <toidi357@gmail.com>
2025-03-26 23:31:43 -0700

    update 3/26/2025

--- urlrewrite.py
+++ urlrewrite.py
@@ -9,7 +9,7 @@ def process_html(file_path):
 
     # use javascript window to perform a URL rewrite
     # essentially if user requests /about.html, it will show /about in the URL bar
-    new_path = file_path.replace("./", "")
+    new_path = file_path.replace("./website/", "")
     if 'index.html' in file_path:
       content = content.replace('</head>', "<script>if (window.location.pathname === '/index.html') {window.history.pushState({}, '', '/')};</script></head>")
     else:
